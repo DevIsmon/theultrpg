0c41382281417e04ece62fc9a2cbca540867acc0
DevIsmon <simon.refuerzo.dev@gmail.com>
2025-10-28 20:28:20 +0800

    Add files via upload

---
+++ src/main/java/com/crowndev/theultrpg/repositories/TheultrpgApplication.java
@@ -0,0 +1,13 @@
+package com.crowndev.theultrpg.repositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheultrpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheultrpgApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/crowndev/theultrpg/TheultrpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowndev.theultrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheultrpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
